782893a62ccd206fd44325153f6ff09dcff3dcc9
Stampede2011 <carterblowers01@gmail.com>
2020-10-31 16:58:19 -0500

    Fixing!

--- src/main/java/co/carrd/andwhat5/sts/config/STSConfig.java
+++ src/main/java/co/carrd/andwhat5/sts/config/STSConfig.java
@@ -1,39 +1,75 @@
 package co.carrd.andwhat5.sts.config;
-
+import com.google.common.collect.Lists;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
+
+import java.util.List;
 
 @ConfigSerializable
 public class STSConfig {
+
+
     @Setting
-    General general = new General();
+    Messages messages = new Messages();
 
     @ConfigSerializable
-    public static class General {
-        @Setting(comment="The amount of money per level of the Pokemon.")
+    public static class Messages
+    {
+        @Setting
+        public static String prefix = "&8[&a&lSTS&8] &f";
+
+        @Setting
+        public static String lastPokemon = "&cYou can't sell your last Pokemon!";
+
+        @Setting
+        public static String missingPokemon = "&cError selling %pokemon%. Is it still in your party?";
+
+        @Setting
+        public static String sellPokemon = "&aYou sold your %pokemon% for $%price%!";
+
+        @Setting
+        public static String reload = "&aPlugin has been reloaded!";
+
+        @Setting
+        public static String loadError = "&cCould not load your party! Please try again...";
+
+    }
+
+    @Setting(comment = "All available boosters to modify the price. Set to 0 to disable a booster.")
+    Boosters boosters = new Boosters();
+
+    @ConfigSerializable
+    public static class Boosters
+    {
+        @Setting(comment = "The amount of money per level of the Pokemon.")
         public static int moneyPerLevel = 15;
-        @Setting(comment="The amount of money per % of the Pokemons iv. Example: $5 for a 1% IV Pokemon, $10 for a 2% IV Pokemon.")
+
+        @Setting(comment = "The amount of money if the Pokemon is level 100.")
+        public static int maxLevel = 1000;
+
+        @Setting(comment = "The amount of money per % of the Pokemons ivs. Example: $5 for a 1% IV Pokemon, $10 for a 2% IV Pokemon.")
         public static int moneyPerIV = 5;
-        @Setting(comment="The amount of money given if the Pokemon has a Hidden Ability.")
-        public static int hiddenAbilityBooster = 1000;
-        @Setting(comment="The amount of money given if the Pokemon has perfect IVs.")
+
+        @Setting(comment = "The amount of money per % of the Pokemons evs. Example: $5 for a 1% EV Pokemon, $10 for a 2% EV Pokemon.")
+        public static int moneyPerEV = 5;
+
+        @Setting(comment = "The amount of money given if the Pokemon has perfect IVs.")
         public static int perfectIVBooster = 1000;
-        @Setting(comment="The amount of money given if the Pokemon is shiny.")
+
+        @Setting(comment = "The amount of money given if the Pokemon has perfect EVs.")
+        public static int perfectEVBooster = 1000;
+
+        @Setting(comment = "The amount of money given if the Pokemon has a Hidden Ability.")
+        public static int hiddenAbilityBooster = 1000;
+
+        @Setting(comment = "The amount of money given if the Pokemon is shiny.")
         public static int shinyBooster = 2000;
-        @Setting(comment="The amount of money given if the Pokemon is a legendary.")
+
+        @Setting(comment = "The amount of money given if the Pokemon is a legendary.")
         public static int legendaryBooster = 4000;
-<<<<<<< Updated upstream
-        @Setting(comment="The amount of money given to the player if the Pokemon has a custom texture.")
-        public static int customTextureBooster = 1000;
-        @Setting(comment="Allow the sales of eggs (Note: Players will be able to see the stats/type of the egg)")
-        public static boolean allowEggs = false;
-        @Setting(comment="The amount of money given if the Pokemon is an ultrabeast.")
-        public static int ultraBeastBooster = 2500;
-        @Setting(comment="The amount of money given if the Pokemon has perfect EVs.")
-        public static int perfectEVBooster = 500;
-        @Setting(comment="The amount of money given if the Pokemon is max level.")
-        public static int MaxLevelBooster = 500;
-=======
 
         @Setting(comment = "The amount of money given to the player if the Pokemon has a special texture.")
         public static int specialTextureBooster = 1000;
@@ -58,13 +94,13 @@ public class STSConfig {
         public static class Pokemon
         {
             @Setting(comment = "The name of the item for each Pokemon. %pokemon% is replaced with the Pokemon name.")
-            public static String displayName = "&6%pokemon%";
+            public static String displayName = "&6&l%pokemon%";
 
             @Setting(comment = "The format of each booster in the lore. %booster% is replaced with the Booster name and %price% is replaced with the price.")
             public static String loreBooster = "&a%booster%: $&b%price%";
 
             @Setting(comment = "The format of the total in the lore. %total% is replaced with the total of all Boosters.")
-            public static String loreTotal = "&aTotal: $&b%total%";
+            public static String loreTotal = "&6Total: &b$%total%";
         }
 
         @Setting(comment = "The item displayed when there is no Pokemon in that slot")
@@ -80,7 +116,7 @@ public class STSConfig {
             public static int unsafeDamage = 0;
 
             @Setting
-            public static String displayName = "";
+            public static String displayName = "&c&lNo Pokemon Found!";
 
             @Setting
             public static List<String> lore = Lists.newArrayList();
@@ -124,33 +160,6 @@ public class STSConfig {
             public static List<String> lore = Lists.newArrayList();
         }
 
-//        @Setting(comment = "The amount of money given to the player if the Pokemon has a special texture.")
-//        Border paneTop = new Border();
-//
-//        @Setting(comment = "The amount of money given to the player if the Pokemon has a special texture.")
-//        Border paneMiddle = new Border();
-//
-//        @Setting(comment = "The amount of money given to the player if the Pokemon has a special texture.")
-//        Border paneBottom = new Border();
-//
-//        @ConfigSerializable
-//        public static class Border
-//        {
-//            @Setting
-//            public static ItemType itemType = ItemTypes.STAINED_GLASS_PANE;
-//
-//            @Setting
-//            public static int unsafeDamage = 0;
-//
-//            @Setting
-//            public static String displayName = "";
-//
-//            @Setting
-//            public static List<String> lore = Lists.newArrayList();
-//        }
-
->>>>>>> Stashed changes
     }
 
 }
-
